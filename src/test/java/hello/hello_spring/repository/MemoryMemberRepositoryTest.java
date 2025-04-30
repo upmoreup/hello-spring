8ce4934bae6cb54f0c713c8a64e4c46334887b9c
grabnpush <grabnpush@gmail.com>
2025-04-30 12:04:08 +0900

    feat: SpringBoot 유저정보 저장 메서드 및 테스트 케이스 작성 전까지 작성

---
+++ src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,13 @@
+package hello.hello_spring.repository;
+
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+
+    }
+}
